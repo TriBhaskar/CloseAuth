1392517e70c878cfbaf1ee3185dc87f8a0e7bbcd
TriBhaskar <panthribhaskar@gmail.com>
2025-08-20 22:50:55 +0530

    Initialize Spring Boot project with Maven configuration and basic structure

---
+++ closeauth-backend/src/main/java/com/anterka/closeauthbackend/CloseauthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anterka.closeauthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloseauthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloseauthBackendApplication.class, args);
+	}
+
+}
---
+++ closeauth-backend/src/test/java/com/anterka/closeauthbackend/CloseauthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anterka.closeauthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloseauthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
